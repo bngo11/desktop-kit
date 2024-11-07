76396172f63dfc1ee834d615ca15e20754f3f1a8
shadow <shadowngoz@gmail.com>
2024-11-07 07:02:37 -0600

    Autogenerated tree updates.
    merged:
    
      flora: 523a446b976b43c4ede7fd49048643aaf4812e42
      faustoo: 962b77da4f92c74fa70933968b2d5b88766b0ca9
      gentoo-staging: 9292583023a6610905c79a4707db3ac6c63b3a31
      kit-fixups: 30dfef72b43baf10ce261c257f9486c6c2ed181f

--- app-admin/passwordsafe/autogen.py
+++ app-admin/passwordsafe/autogen.py
@@ -1,24 +1,42 @@
 #!/usr/bin/env python3
 
 import json
-from re import match
-
 
 async def generate(hub, **pkginfo):
-	json_list = await hub.pkgtools.fetch.get_page("https://api.github.com/repos/pwsafe/pwsafe/releases", is_json=True)
-	for release in json_list:
-		if release["prerelease"] or release["draft"]:
-			continue
-		if "Linux" not in release["name"]:
-			continue
-		version = release["tag_name"]
-		url = f"https://github.com/pwsafe/pwsafe/archive/{version}.tar.gz"
-		final_name = f'{pkginfo["name"]}-{version}.tar.gz'
-		break
-	ebuild = hub.pkgtools.ebuild.BreezyBuild(
-		**pkginfo, version=version, artifacts=[hub.pkgtools.ebuild.Artifact(url=url, final_name=final_name)]
-	)
-	ebuild.push()
+	json_data = await hub.pkgtools.fetch.get_page("https://api.github.com/repos/pwsafe/pwsafe/releases", is_json=True)
+	version = None
 
+	for item in json_data:
+		try:
+			if item["prerelease"] or item["draft"]:
+				continue
+
+			version = item["tag_name"]
+			list(map(int, version.split(".")))
+			break
+
+			'''
+			for asset in item['assets']:
+				asset_name = asset["name"]
+
+				if asset_name.endswith("tar.gz"):
+					url = asset["browser_download_url"]
+					break
+
+			if url:
+				break
+			'''
+
+		except (KeyError, IndexError, ValueError):
+			continue
+
+	if version:
+		url = f"https://github.com/pwsafe/pwsafe/archive/{version}.tar.gz"
+		ebuild = hub.pkgtools.ebuild.BreezyBuild(
+			**pkginfo,
+			version=version,
+			artifacts=[hub.pkgtools.ebuild.Artifact(url=url, final_name=url.rsplit('/', 1)[-1])]
+		)
+		ebuild.push()
 
 # vim: ts=4 sw=4 noet
