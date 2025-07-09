70a8675d439756db4c6b022fafbf9cb5c8938b0e
shadow <shadowngoz@gmail.com>
2025-07-09 16:10:05 -0500

    Autogenerated tree updates.
    merged:
    
      flora: 523a446b976b43c4ede7fd49048643aaf4812e42
      faustoo: 962b77da4f92c74fa70933968b2d5b88766b0ca9
      gentoo-staging: 9292583023a6610905c79a4707db3ac6c63b3a31
      kit-fixups: f85bdf9b5411b5b6af761035605645abb093dad3

--- net-print/hplip-plugin/hplip-plugin-3.25.2.ebuild
+++ net-print/hplip-plugin/hplip-plugin-3.25.2.ebuild
@@ -7,14 +7,14 @@ inherit udev unpacker
 
 DESCRIPTION="Proprietary plugins and firmware for HPLIP"
 HOMEPAGE="https://developers.hp.com/hp-linux-imaging-and-printing/plugins"
-SRC_URI="https://developers.hp.com/sites/default/files/hplip-3.24.4-plugin.run -> hplip-3.24.4-plugin.run"
+SRC_URI="https://www.openprinting.org/download/printdriver/auxfiles/HP/plugins/hplip-3.25.2-plugin.run -> hplip-3.25.2-plugin.run"
 LICENSE="hplip-plugin"
 SLOT="0"
 KEYWORDS="*"
 IUSE=""
 
 RDEPEND="
-	>=net-print/hplip-3.24.0
+	>=net-print/hplip-3.25.2
 	virtual/udev
 "
 DEPEND=""
--- net-print/hplip/autogen.py
+++ net-print/hplip/autogen.py
@@ -1,21 +1,37 @@
 #!/usr/bin/env python3
 
-import json
-import re
-
+from bs4 import BeautifulSoup
 
 async def generate(hub, **pkginfo):
-	hplip_url = "https://developers.hp.com/hp-linux-imaging-and-printing/release_notes"
-	dist_url = "https://sourceforge.net/projects/hplip/files/hplip/{0}/hplip-{0}.tar.gz"
-	patch_url = "https://dev.gentoo.org/~billie/distfiles/hplip-{0}-patches-1.tar.xz"
-	hplip_data = await hub.pkgtools.fetch.get_page(hplip_url)
-	version = re.findall(r"HPLIP (\d+\.\d+\.\d+)", hplip_data)[0]
-	hplip_version = re.findall(r"(\d+\.\d+).\d+", version)[0] + ".0"
-	ebuild = hub.pkgtools.ebuild.BreezyBuild(
-		**pkginfo,
-		version=version,
-		hplip_version=hplip_version,
-		artifacts=[hub.pkgtools.ebuild.Artifact(url=dist_url.format(version)),
-					hub.pkgtools.ebuild.Artifact(url=patch_url.format(version))],
-	)
-	ebuild.push()
+	html_data = await hub.pkgtools.fetch.get_page("https://sourceforge.net/projects/hplip/files/hplip/")
+	soup = BeautifulSoup(html_data, "html.parser")
+	links = soup.find_all("a")
+	version = None
+
+	for link in links:
+		href = link.get("href")
+		if href and "files" in href:
+			parts = href.split("/")
+			version = parts[-2]
+
+			try:
+				list(map(int, version.split(".")))
+				break
+
+			except ValueError:
+				continue
+
+	if version:
+		url = f"https://sourceforge.net/projects/hplip/files/hplip/{version}/hplip-{version}.tar.gz"
+		patch_url = f"https://dev.gentoo.org/~billie/distfiles/hplip-{version}-patches-1.tar.xz"
+		ebuild = hub.pkgtools.ebuild.BreezyBuild(
+			**pkginfo,
+			version=version,
+			artifacts=[hub.pkgtools.ebuild.Artifact(url=url),
+						hub.pkgtools.ebuild.Artifact(url=patch_url)],
+		)
+
+		ebuild.push()
+
+
+# vim: ts=4 sw=4 noet
